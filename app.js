0f49c5cc8faf9af5aa690c68c1b5478bdf08d27e
Ellie-Aghajani <z.aqajani@gmail.com>
2024-05-08 17:10:07 -0700

    update books data

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
+const { boolean } = require('joi');
 const mongoose= require('mongoose');
 
 mongoose.connect('mongodb://localhost/publications')
     .then(() => console.log('Connected to mongodb'))
     .catch(err => console.log(err.message));
  
+const bookSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageURL:String,
+    tags:[String],
+    stock: Number,
+    date:{ type: Date, default: Date. now }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+async function createBook(){
+    try{
+        const book = new Book({
+            title: 'first book',
+            description: "psychology",
+            imageURL:String,
+            tags:['adults'],
+            stock: 1
+        });
+        const result = await book.save();
+        console.log(result);
+
+    }
+    catch(err){
+        console.log('Error creating book', err.message);
+    }
+};
+// createBook();
+async function getBooks(){
+    return await Book.find();
+
+}
+
+async function run(){
+    const books = await getBooks();
+    console.log(books);
+};
+
+run();
